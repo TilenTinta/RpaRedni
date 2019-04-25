c45001548a0321eaddd2931a2258ff1bae20b6da
TilenT <tilen.tinta@gmail.com>
2019-04-25 14:53:03 +0200

    25.4.2019

---
+++ Primer_WebApi/Primer_WebApi/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Primer_WebApi.Models
+{
+    public class Product
+    {
+        [ScaffoldColumn(false)]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal ActualCost { get; set; }
+    }
+}
